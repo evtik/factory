c24c6a754b6bd730ff8b0f5a9fe5f24ce9bf62ca
Serhii <e.v.t.i.k@ukr.net>
2016-11-28 16:34:47 +0200

    Remove unnecessary send method calls

--- factory.rb
+++ factory.rb
@@ -1,23 +1,23 @@
 class Factory
   def self.new(*attrs, &block)
     Class.new do
-      send(:attr_accessor, *attrs)
+      attr_accessor(*attrs)
 
-      send(:define_method, :initialize) do |*args|
+      define_method :initialize do |*args|
         args.each_with_index do |arg, i|
           send("#{attrs[i]}=", arg)
         end
       end
 
-      send(:define_method, :[]) do |arg|
+      define_method :[] do |arg|
         arg.is_a?(Integer) ? send(attrs[arg]) : send(arg)
       end
 
-      send(:define_method, :[]=) do |arg, val|
+      define_method :[]= do |arg, val|
         arg.is_a?(Integer) ? send("#{attrs[arg]}=", val) : send("#{arg}=", val)
       end
 
-      send(:class_eval, &block) if block_given?
+      class_eval(&block) if block_given?
     end
   end
 end
