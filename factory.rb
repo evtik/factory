fb8946aec7dada915798141939e0b1b52aed92fc
Serhii <e.v.t.i.k@ukr.net>
2016-11-30 11:05:18 +0200

    Add check for args number

--- factory.rb
+++ factory.rb
@@ -4,17 +4,19 @@ class Factory
       attr_accessor(*attrs)
 
       define_method :initialize do |*args|
+        raise ArgumentError, 'struct size differs' if args.length > attrs.length
+
         args.each_with_index do |arg, i|
-          send("#{attrs[i]}=", arg)
+          send "#{attrs[i]}=", arg
         end
       end
 
       define_method :[] do |arg|
-        arg.is_a?(Integer) ? send(attrs[arg]) : send(arg)
+        arg.is_a?(Numeric) ? send(attrs[arg]) : send(arg)
       end
 
       define_method :[]= do |arg, val|
-        arg.is_a?(Integer) ? send("#{attrs[arg]}=", val) : send("#{arg}=", val)
+        arg.is_a?(Numeric) ? send("#{attrs[arg]}=", val) : send("#{arg}=", val)
       end
 
       class_eval(&block) if block_given?
